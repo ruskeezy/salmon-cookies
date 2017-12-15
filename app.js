1d3a58457db624f5b79e7b7c1298216c3bbe9291
ruskeezy <christian.ax.miller@gmail.com>
2017-12-14 20:10:06 -0800

    added form in html, and styles from demo

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var storeLocations = [];
+var form = document.getElementById('store-form');
 var storeTable = document.getElementById('stores')
 
 
